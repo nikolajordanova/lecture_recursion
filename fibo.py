455a3e71e6d9b4e336a3a68979b65e3853dc9f97
Nikola Jordanova <nikolajordanova55@gmail.com>
2023-04-24 09:37:36 +0200

    Fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,15 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
-    pass
+    n_fibo = recursive_nth_fibo(4)
+    print(n_fibo)
 
 
 if __name__ == "__main__":
